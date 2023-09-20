1bb1d16b47dfc11bc7d4e4285242fd0a34532a10
Impronoucabl <Impronoucabl@gmail.com>
2023-09-20 21:54:05 +1000

    Got SVG example paths & circles working

--- svg_gal/src/main.rs
+++ svg_gal/src/main.rs
@@ -1,17 +1,20 @@
 use std::env;
 use svg::Document;
-use svg::node::element::Path;
+use svg::node::element::{Path, Circle};
 use svg::node::element::path::Data;
 
 fn main() {
+    let WIDTH = 512;
+    let HEIGHT = 512;
     let args: Vec<String> = env::args().collect();
     let raw_text = &args[1];
     let seed_text = &args[2];
     let _seed = seed_text.to_owned().into_bytes();
     println!("Start");
     let data = Data::new()
-        .move_to((-10, -10))
-        .line_by((0, 50))
+        .move_to((&WIDTH/2, 10))
+        // x radius, y radius, rotation, large arc, sweep direction
+        .elliptical_arc_by((30,30, 0,0,0,250,250))
         .line_by((50, 0))
         .line_by((0, -50))
         .close();
@@ -22,9 +25,18 @@ fn main() {
         .set("stroke-width", 3)
         .set("d", data);
 
+    let circle = Circle::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("cx", &WIDTH/2)
+        .set("cy", &HEIGHT/2)
+        .set("r", 40);
+
     let document = Document::new()
-        .set("viewBox", (0, 0, 512, 512))
-        .add(path);
+        .set("viewBox", (0, 0, WIDTH, HEIGHT))
+        .add(path)
+        .add(circle);
     println!("Saving");
     svg::save(raw_text.to_owned() + ".svg", &document).unwrap();
 }
