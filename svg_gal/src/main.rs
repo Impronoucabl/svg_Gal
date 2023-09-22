16e78cf4235973c2f7ecd94793f7406ff25b13f8
Impronoucabl <Impronoucabl@gmail.com>
2023-09-23 09:36:15 +1000

    Added skelepath func

--- svg_gal/src/main.rs
+++ svg_gal/src/main.rs
@@ -3,6 +3,23 @@ use svg::Document;
 use svg::node::element::{Path, Circle};
 use svg::node::element::path::Data;
 
+fn skele_path(skele_str: &str, WIDTH: &i32) -> Path {
+    print!("{}", skele_str);
+    let data = Data::new()
+        .move_to((WIDTH/2, 10))
+        // x radius, y radius, rotation, large arc, sweep direction
+        .elliptical_arc_by((30,30, 0,0,0,250,250))
+        .line_by((50, 0))
+        .line_by((0, -50))
+        .close();
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("d", data);
+    path
+}
+
 fn main() {
     let WIDTH = 512;
     let HEIGHT = 512;
@@ -11,19 +28,9 @@ fn main() {
     let seed_text = &args[2];
     let _seed = seed_text.to_owned().into_bytes();
     println!("Start");
-    let data = Data::new()
-        .move_to((&WIDTH/2, 10))
-        // x radius, y radius, rotation, large arc, sweep direction
-        .elliptical_arc_by((30,30, 0,0,0,250,250))
-        .line_by((50, 0))
-        .line_by((0, -50))
-        .close();
-
-    let path = Path::new()
-        .set("fill", "none")
-        .set("stroke", "black")
-        .set("stroke-width", 3)
-        .set("d", data);
+    
+    let skele_str = "zoo";
+    let skele = skele_path(skele_str, &WIDTH);
 
     let circle = Circle::new()
         .set("fill", "none")
@@ -35,7 +42,7 @@ fn main() {
 
     let document = Document::new()
         .set("viewBox", (0, 0, WIDTH, HEIGHT))
-        .add(path)
+        .add(skele)
         .add(circle);
     println!("Saving");
     svg::save(raw_text.to_owned() + ".svg", &document).unwrap();
