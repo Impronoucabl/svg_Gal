e667e26edede4568f9978eea02fafd8209b3c7ab
Impronoucabl <Impronoucabl@gmail.com>
2023-09-23 17:35:32 +1000

    Pre-Theta_Thi

--- svg_gal/src/gall_struct.rs
+++ svg_gal/src/gall_struct.rs
@@ -1,21 +1,21 @@
 pub struct GallCircle<'loc> { //Syllable equivalent
-    character: char,
-    repeat: bool,
-    vowel:Option<VowCircle>,
-    loc: GallOrd<'loc>,
-    radius: f64,
-    decorators:Vec<Decor<'loc>>
+    pub character: char,
+    pub repeat: bool,
+    pub vowel:Option<VowCircle>,
+    pub loc: GallOrd<'loc>,
+    pub radius: f64,
+    pub decorators:Vec<Decor<'loc>>
 }
 
-struct VowCircle { //for attached vowels only
-    character: char,
-    repeat: bool,
-    radius: f64,
+pub struct VowCircle { //for attached vowels only
+    pub character: char,
+    pub repeat: bool,
+    pub radius: f64,
 }
 
-struct Decor<'loc> {
-    loc: GallOrd<'loc>,
-    dot: bool,
+pub struct Decor<'loc> {
+    pub loc: GallOrd<'loc>,
+    pub dot: bool,
 }
 
 use std::f64::consts::PI;
@@ -27,6 +27,15 @@ pub struct GallOrd <'parent> {
     pub parent: Option<&'parent GallOrd<'parent>>,
 }
 
+impl GallCircle<'_> {
+    pub fn theta(&self) -> f64 {
+        1.1
+    }
+    pub fn thi(&self) -> f64 {
+        0.6
+    }
+}
+
 impl GallOrd<'_> {
     fn svg_x(&self) -> f64 {
         match self.ang {
@@ -36,22 +45,31 @@ impl GallOrd<'_> {
     }
     fn svg_y(&self) -> f64 {
         match self.ang {
-            Some(rad) => self.dist*(rad - PI/2.0).sin() + self.center.1,
+            Some(rad) => self.dist*(rad - PI/2.0).sin() - self.center.1,
             None => self.center.1
         }
     }
-    fn set_ang_d(&mut self, co_ord: (f64,f64)) {
+    pub fn svg_ord(&self) -> (f64,f64) {
+        match self.ang {
+            Some(rad) => (
+                self.dist*(rad - PI/2.0).cos() + self.center.0,
+                self.dist*(rad - PI/2.0).sin() - self.center.1
+            ),
+            None => self.center
+        }
+    }
+    pub fn set_ang_d(&mut self, co_ord: (f64,f64)) {
         self.ang = Some(co_ord.0);
         self.dist = co_ord.1;
     }
-    fn clockwise(&mut self, radians:f64) {
+    pub fn clockwise(&mut self, radians:f64) {
         let ang= match self.ang {
             Some(rad) => rad,
             None => 0.0,
         };
         self.ang = Some(ang - radians);
     }
-    fn c_clockwise(&mut self, radians:f64) {
+    pub fn c_clockwise(&mut self, radians:f64) {
         let ang= match self.ang {
             Some(rad) => rad,
             None => 0.0,
--- svg_gal/src/main.rs
+++ svg_gal/src/main.rs
@@ -6,26 +6,54 @@ use svg::node::element::path::Data;
 
 mod gall_struct;
 
-fn render_skele_path(skeleton_letters:Vec<gall_struct::GallCircle>, svg_doc:Document, &WIDTH: &i32, &HEIGHT: &i32) -> SVG {
+fn text_to_gall(text:&String) -> Vec<gall_struct::GallCircle> {
+    let mut syllable_list = Vec::new();
+    for letter in text.chars() {
+        syllable_list.push(
+            gall_struct::GallCircle{
+                character: letter,
+                repeat: false,
+                vowel: None,
+                loc:
+                    gall_struct::GallOrd { 
+                        ang: Some(0.0), 
+                        dist: 10.0, 
+                        center: (30.0,30.0), 
+                        parent: None 
+                    },
+                radius: 100.0,
+                decorators: Vec::new(),
+            }
+        )
+    }
+    syllable_list
+}
+
+fn render_skele_path(skeleton_letters:Vec<gall_struct::GallCircle>, svg_doc:Document, loc: &mut gall_struct::GallOrd) -> SVG {
     if skeleton_letters.len() == 0 {
         let circle = Circle::new()
             .set("fill", "none")
             .set("stroke", "black")
             .set("stroke-width", 3)
-            .set("cx", WIDTH/2)
-            .set("cy", HEIGHT/2)
+            .set("cx", loc.center.0/2.0)
+            .set("cy", loc.center.1/2.0)
             .set("r", 200);
         svg_doc.add(circle)
     } else {
+        loc.set_ang_d((0.0,loc.center.0));
+        if false { //skeleton_letters[0].character == '_'
+            loc.ang = Some(-0.1);
+        }
         let skele_data = Data::new()
-            .move_to((10,10))
+            .move_to(loc.svg_ord())
             // x radius, y radius, rotation, large arc, sweep direction
-            .elliptical_arc_by((30,30, 0,0,0,250,250))
-            .line_by((50, 0))
-            .line_by((0, -50))
+            .elliptical_arc_by((0,0, 0,0,0,250,250))
             .close();
+
+
+        //attach path to svg
         let path = Path::new()
-            .set("fill", "none")
+            .set("fill", "green")
             .set("stroke", "black")
             .set("stroke-width", 3)
             .set("d", skele_data);
@@ -33,7 +61,7 @@ fn render_skele_path(skeleton_letters:Vec<gall_struct::GallCircle>, svg_doc:Docu
     }
 }
 
-fn render_lttr_path(syllables:Vec<gall_struct::GallCircle>, svg_doc:Document, &WIDTH: &i32, &HEIGHT: &i32) -> SVG {
+fn render_lttr_path(syllables:Vec<gall_struct::GallCircle>, svg_doc:Document, loc: &gall_struct::GallOrd) -> SVG {
     if syllables.len() == 0 {
         let skele_data = Data::new()
             .move_to((10,10))
@@ -43,45 +71,38 @@ fn render_lttr_path(syllables:Vec<gall_struct::GallCircle>, svg_doc:Document, &W
             .line_by((0, -50))
             .close();
         let path2 = Path::new()
-            .set("fill", "blue")
+            .set("fill", "none")
             .set("stroke", "black")
             .set("stroke-width", 3)
             .set("d", skele_data);
         svg_doc.add(path2)
     } else {
-        print!("something");
         svg_doc
     }
 }
 
 fn main() {
-    let WIDTH = 512;
-    let HEIGHT = 512;
+    let WIDTH = 512.0;
+    let HEIGHT = 512.0;
     let args: Vec<String> = env::args().collect();
     let raw_text = &args[1];
     let seed_text = &args[2];
     let _seed = seed_text.to_owned().into_bytes();
     println!("Start");
-
-    let skele_ltrs = Vec::new();
+    let all_letters = text_to_gall(raw_text);
+    let skele_ltrs = all_letters;//Vec::new();
     let oth_ltrs = Vec::new();
 
-    let skele_data = Data::new()
-        // x radius, y radius, rotation, large arc, sweep direction
-        .elliptical_arc_by((10,10, 0,0,0,250,250))
-        .line_by((150, 0))
-        .line_by((0, -50))
-        .close();
-    let path3 = Path::new()
-        .set("fill", "blue")
-        .set("stroke", "black")
-        .set("stroke-width", 3)
-        .set("d", skele_data);
-
+    let mut origin = gall_struct::GallOrd{
+        ang: None,
+        dist: 0.0,
+        center: (WIDTH/2.0,HEIGHT/2.0),
+        parent: None,
+    };
     let document = Document::new()
         .set("viewBox", (0, 0, WIDTH, HEIGHT));
-    let skeleton = render_skele_path(skele_ltrs, document,&WIDTH, &HEIGHT);
-    let all_letters = render_lttr_path(oth_ltrs, skeleton, &WIDTH, &HEIGHT);
+    let skeleton = render_skele_path(skele_ltrs, document, &mut origin);
+    let all_letters = render_lttr_path(oth_ltrs, skeleton, &origin);
     println!("Saving");
     svg::save(raw_text.to_owned() + ".svg", &all_letters).unwrap();
 }
