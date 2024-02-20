7169535a092773fda3e6e1cf7f4e4028bce58d33
Impronoucabl <Impronoucabl@gmail.com>
2024-02-20 11:35:00 +1100

    Removed commented out thickness

--- svg_gal/src/render.rs
+++ svg_gal/src/render.rs
@@ -66,11 +66,7 @@ impl GallWord {
         let stem1 = skel[0].stem.first().unwrap();
         let center_ref = stem1.get_center();
         let w_rad = stem1.parent_radius();
-        //let w_in_rad = stem1.parent_inner();
-        //let w_ou_rad = stem1.parent_outer();
         let mut l_rad = stem1.radius();
-        //let mut l_in_rad = stem1.inner_radius();
-        //let mut l_ou_rad = stem1.outer_radius();
         let mut l_dist = stem1.dist();
         
         let mut b_div_flag = match stem1.stem_type {
@@ -78,66 +74,24 @@ impl GallWord {
             StemType::S => {l_dist += stem1.thick(); 0},//change to word.thick()?
             _ => {panic!()}
         };
-        // if stem1.parent_inner() - l_dist - l_ou_rad >= 0.0 {
-        //     panic!("Letter not touching outer skeleton");
-        // }
-        // if stem1.parent_outer() - l_dist - l_in_rad >= 0.0 {
-        //     panic!("Letter not touching inner skeleton");
-        // }
         let mut thi = gall_fn::thi(
             l_dist,
             l_rad, 
             w_rad,
         );
-        // let mut thi_inner = gall_fn::thi(
-        //     l_dist,
-        //     l_ou_rad, 
-        //     stem1.parent_inner(),
-        // );
-        // let mut thi_outer = gall_fn::thi(
-        //     l_dist,
-        //     l_in_rad, 
-        //     stem1.parent_outer(),
-        // );
         let mut word_end_angle = stem1.ang().unwrap() - thi;
-        //let mut inner_word_end_angle = stem1.ang().unwrap() - thi_inner;
-        //let mut outer_word_end_angle = stem1.ang().unwrap() - thi_outer;
         let init_angle = 0.0_f64.min(word_end_angle);
-        // let inner_init_angle = 0.0_f64.min(inner_word_end_angle);
-        // let outer_init_angle = 0.0_f64.min(outer_word_end_angle);
         let mut tracker = GallLoc::new(
             init_angle,
             w_rad,
             center_ref,
         );
-        // let mut inner_tracker = GallLoc::new(
-        //     inner_init_angle,
-        //     w_in_rad,
-        //     center_ref,
-        // );
-        // let mut outer_tracker = GallLoc::new(
-        //     outer_init_angle,
-        //     w_out_rad,
-        //     center_ref,
-        // );
         let continuum = tracker.svg_ord();
-        // let inner_continuum = inner_tracker.pos_ref().get();
-        // let outer_continuum = outer_tracker.pos_ref().get();
         let mut long_skeleton = if word_end_angle > PI {1} else {0};
-        //let mut long_inner_skeleton = if inner_word_end_angle > PI {1} else {0};
-        //let mut long_outer_skeleton = if outer_word_end_angle > PI {1} else {0};
         tracker.mut_ang(word_end_angle);
-        // inner_tracker.mut_ang( inner_word_end_angle);
-        // outer_tracker.mut_ang( outer_word_end_angle);
         let mut letter_start = tracker.svg_ord();
-        // let mut inner_letter_start = inner_tracker.svg_ord();
-        // let mut outer_letter_start = outer_tracker.svg_ord();
         tracker.mut_ccw(2.0 * thi);
-        // inner_tracker.mut_ccw(2.0 * thi_inner);
-        // outer_tracker.mut_ccw(2.0 * thi_outer);
         let mut letter_finish = tracker.svg_ord();
-        // let mut inner_letter_finish = inner_tracker.svg_ord();
-        // let mut outer_letter_finish = outer_tracker.svg_ord();
 
         //TODO: Put this in part rend? -------------------------------
         let mut data = Data::new()
@@ -167,17 +121,11 @@ impl GallWord {
         }
 
         let sweep = if tracker.ang().unwrap() - init_angle > PI {0} else {1};
-        // let mut inner_sweep = if inner_tracker.ang().unwrap() - inner_init_angle > PI {0} else {1};
-        // let mut outer_sweep = if outer_tracker.ang().unwrap() - outer_init_angle > PI {0} else {1};
         let closed_loop = data
             .elliptical_arc_to((w_rad,w_rad,0,sweep,0,continuum.0,continuum.1))
             .close();
         let path = Path::new()
             .set("d", closed_loop);
-        // let inner_path = Path::new()
-        //     .set("d", closed_inner_loop);
-        // let outer_path = Path::new()
-        //     .set("d", closed_outer_loop);
         drawn = drawn.add(path
             .set("fill", "green")
             .set("stroke-width", 5.0)
